e91fef5895fac41c3fc3d8d15c9175cc23097477
pedrofanti <87535935+pedrofanti@users.noreply.github.com>
2023-05-08 19:00:48 -0300

    Create urls.py

---
+++ urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from .views import CursoAPIView, AvaliacoesAPIView, AvaliacaoAPIView, CursosAPIView
+
+urlpatterns = [
+    path('cursos/', CursosAPIView.as_view(), name='cursos'),
+    path('avaliacoes/', AvaliacoesAPIView.as_view(), name='avaliacoes'),
+    path('cursos/<int:pk>', CursoAPIView.as_view(), name='curso'),
+    path('avaliacoes/<int:pk>', AvaliacaoAPIView.as_view(), name='avaliacao'),
+    ]
+
